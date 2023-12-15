77dcb0722d13597a141ae5f432838163768d70f7
Евгений Рычков <fakerichckov@yandex.ru>
2023-12-16 01:11:12 +0500

    1st review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -14,10 +20,38 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                Follow.objects.all(),
+                ('user', 'following'),
+            ),
+        ]
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'Запрещено подписываться на самого себя'
+            )
+        return data
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import CheckConstraint, F, Q, UniqueConstraint
 
 User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200,)
+    slug = models.SlugField(max_length=200, unique=True,)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -9,8 +22,15 @@ class Post(models.Model):
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(
+        Group, models.SET_NULL,
+        blank=True, null=True, related_name='posts',
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+
+    class Meta:
+        ordering = ('id',)
 
     def __str__(self):
         return self.text
@@ -24,3 +44,31 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.text
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        UniqueConstraint(fields=['user', 'following'], name='unique_follow')
+        CheckConstraint(
+            check=~Q(user=F('following')),
+            name='not_follow_yourselfe'
+        )
